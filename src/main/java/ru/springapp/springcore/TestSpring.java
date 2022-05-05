ca55d4a75c1881f2817e7675d09ca9edd5cc37e3
Bill44-NotAlone <denyc9@mail.ru>
2022-05-05 16:23:04 +0300

    Анотация Component

--- src/main/java/ru/springapp/springcore/TestSpring.java
+++ src/main/java/ru/springapp/springcore/TestSpring.java
@@ -1,20 +1,15 @@
 package ru.springapp.springcore;
 
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.stereotype.Component;
 
+@Component
 public class TestSpring {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
                 "appContext.xml"
         );
-        ClassicalMusic classicalMusic = context.getBean("classicalMusic", ClassicalMusic.class);
-        System.out.println(classicalMusic);
-        classicalMusic = context.getBean("classicalMusic", ClassicalMusic.class);
-        System.out.println(classicalMusic);
-        RockMusic rockMusic = context.getBean("rockMusic", RockMusic.class);
-        System.out.println(rockMusic);
-        rockMusic = context.getBean("rockMusic", RockMusic.class);
-        System.out.println(rockMusic);
-        context.close();
+        ClassicalMusic music = context.getBean("classicalMusic", ClassicalMusic.class);
+        System.out.println(music.getSong());
     }
 }
